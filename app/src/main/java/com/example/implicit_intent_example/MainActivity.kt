01a4cf0c4b7496b0dcd1a2a49922b18f6cd1b7a7
Abdul Waheed <abdul.bcss19@iba-suk.edu.pk>
2022-04-01 10:35:27 +0500

    design done

---
+++ app/src/main/java/com/example/implicit_intent_example/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.implicit_intent_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnCall = findViewById<Button>(R.id.btnCall)
+        val txtPhoneNum = findViewById<EditText>(R.id.txtPhoneNum)
+
+    }
+}
